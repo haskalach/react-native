fb67d11a45306815f039f0f30679c669624d896f
hassan kalach <hassan_kalach@hotmail.com>
2020-06-05 17:17:27 +0300

    end of chapter 5

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -4,11 +4,11 @@ import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
     return (
-    <View>
-        <ImageDetail title="Forest"/>
-        <ImageDetail title="Beach"/>
-        <ImageDetail title="Mountain"/>
-    </View>
+        <View>
+            <ImageDetail title="Forest" imageSource={require('../../assets/images/forest.jpg')} score={2} />
+            <ImageDetail title="Beach" imageSource={require('../../assets/images/beach.jpg')} score={5} />
+            <ImageDetail title="Mountain" imageSource={require('../../assets/images/mountain.jpg')}  score={9}/>
+        </View>
     );
 };
 
